69d13e5c620751b4ec0089c7688be023b3045f83
yifanWangCoding <yifanwang0521@gmail.com>
2022-11-27 18:13:01 -0800

    streamlit updated file name

--- change_delimiter.py
+++ change_delimiter.py
@@ -1,7 +1,7 @@
 import csv
 
 reader = csv.reader(open("winequality-white.csv", "r"), delimiter=';')
-writer = csv.writer(open("winequality-white.csv-v1.csv", 'w'), delimiter=',')
+writer = csv.writer(open("winequality-white-v1.csv", 'w'), delimiter=',')
 writer.writerows(reader)
 
 print("Delimiter successfully changed")
